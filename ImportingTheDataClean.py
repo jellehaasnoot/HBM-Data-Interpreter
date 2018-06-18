445a2c9a5ad193220cde3e850ccd21e514fd800a
Tim-deJong <Tim-deJong@users.noreply.github.com>
2018-06-18 16:26:31 +0200

    Revert "Revert "Importing_data_clean""
    This reverts commit fa2ec9f990d6d2db29723d37cd72736c841c54f1.

---
+++ ImportingTheDataClean.py
@@ -0,0 +1,192 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import filedialog
+
+# Constants dimensions
+alpha = 68.7
+gamma = 45.5
+beta = 180 - gamma - alpha
+delta = 77
+epsilon = 73
+tau = 45.7
+zeta = 73
+eta = 180 - delta - tau
+ksi = epsilon - beta
+theta = 90 - beta - ksi
+phi = 90 - alpha + ksi
+l_sitting_tube = 0.580
+l_rear_most_angled_tube = (np.sin(np.deg2rad(beta)) * l_sitting_tube) / np.sin(np.deg2rad(alpha))
+l_rear_bracket_COM = np.sin(np.deg2rad(phi)) * l_rear_most_angled_tube
+l_wheelbase = 0.991
+l_front_bracket_COM = l_wheelbase - l_rear_bracket_COM
+
+# Constants areas bike tubes
+A_horz_tube = np.pi * ((34.9 / 2) ** 2 - ((34.9 - 2.6) / 2) ** 2)
+A_sitt_tube = np.pi * ((31.8 / 2) ** 2 - ((31.8 - 3.2) / 2) ** 2)
+A_fron_tube = np.pi * ((5 / 2) ** 2 - ((5 - 2.6) / 2) ** 2)
+A_rear_tube = 97.3
+A_chai_tube = 98.7
+
+# Other constants needed to calculate the force
+mass_cyclist = 83
+gravitational_constant = 9.81
+
+# Calculations for gravitational forces
+F_y4 = (mass_cyclist * gravitational_constant) / (1 + (l_front_bracket_COM / l_rear_bracket_COM))
+F_y1 = mass_cyclist * gravitational_constant - F_y4
+
+# Names columns
+list_of_column_names_strain = ['Horizontal_tube_rosette_1_strain', 'Horizontal_tube_rosette_2_strain',
+                               'Horizontal_tube_rosette_3_strain', 'Sitting_tube_rosette_1_strain',
+                               'Sitting_tube_rosette_2_strain', 'Sitting_tube_rosette_3_strain',
+                               'Front_angled_tube_1_strain', 'Front_angled_tube_2_strain',
+                               'Front_angled_tube_3_strain', 'Drive_train_gauge_strain',
+                               'Chain_tube_gauge_strain', 'Rear_most_angled_tube_strain']
+list_of_column_names_stress = ['Horizontal_tube_rosette_1_stress', 'Horizontal_tube_rosette_2_stress',
+                               'Horizontal_tube_rosette_3_stress', 'Sitting_tube_rosette_1_stress',
+                               'Sitting_tube_rosette_2_stress', 'Sitting_tube_rosette_3_stress',
+                               'Front_angled_tube_1_stress', 'Front_angled_tube_2_stress',
+                               'Front_angled_tube_3_stress', 'Drive_train_gauge_stress',
+                               'Chain_tube_gauge_stress', 'Rear_most_angled_tube_stress',
+                               'Horizontal_tube_nominal_main_stress', 'Sitting_tube_nominal_main_stress',
+                               'Front_angled_tube_nominal_main_stress']
+names = [list_of_column_names_strain, list_of_column_names_stress]
+
+
+class Data:
+    """
+    This class converts the raw data into usable data, there will also be some operations to make this happen in this
+    class. The file will be read and opened in the init method.
+    """
+
+    def __init__(self):
+        root = tk.Tk()
+        root.withdraw()
+        self.file_name = filedialog.askopenfilename(initialdir="C:\\Users\Jelle\PyCharmProjects",
+                                                    title="Select file...",
+                                                    filetypes=(("ASC Files", "*.asc"), ("All Files", "*.*")))
+        self.opened_file = open('DAQTest2_50Hz_+Channel_info.ASC', 'r')
+        self.read_file = self.opened_file.read()
+
+        os.remove('Intermediate_Text_File.txt')
+        new_text_file = open('Intermediate_Text_File.txt', 'w')
+
+        for line in self.read_file:
+            new_text_file.write(line)
+
+        new_text_file = open('Intermediate_Text_File.txt', 'r')
+        self.lines = []
+
+        for line in new_text_file:
+            self.lines.append(line)
+
+        for i in range(38):
+            del self.lines[0]
+
+    def organizing(self):
+        organized_as_single_columns = []
+
+        for i in range(len(self.lines)):
+            organized_as_single_columns.append(self.lines[i].split())
+
+        organized_data = np.array([np.array(j) for j in organized_as_single_columns])
+
+        # Horizontal_tube_rosette_strain
+        organized_data[:, [9, 1]] = organized_data[:, [1, 9]]
+        organized_data[:, [10, 2]] = organized_data[:, [2, 10]]
+        organized_data[:, [11, 3]] = organized_data[:, [3, 11]]
+
+        # Sitting_tube_rosette_strain
+        organized_data[:, [6, 4]] = organized_data[:, [4, 6]]
+        organized_data[:, [7, 5]] = organized_data[:, [5, 7]]
+        organized_data[:, [8, 6]] = organized_data[:, [6, 8]]
+
+        # Front_angled_tube_rosette_strain
+        organized_data[:, [12, 7]] = organized_data[:, [7, 12]]
+        organized_data[:, [13, 8]] = organized_data[:, [8, 13]]
+        organized_data[:, [14, 9]] = organized_data[:, [9, 14]]
+
+        # Single strain gauges - strain
+        organized_data[:, [15, 10]] = organized_data[:, [10, 15]]
+        organized_data[:, [5, 11]] = organized_data[:, [11, 5]]
+        organized_data[:, [4, 12]] = organized_data[:, [12, 4]]
+
+        # Horizontal_tube_rosette_stress
+        organized_data[:, [24, 13]] = organized_data[:, [13, 24]]
+        organized_data[:, [25, 14]] = organized_data[:, [14, 25]]
+        organized_data[:, [26, 15]] = organized_data[:, [15, 26]]
+
+        # Sitting_tube_rosette_stress
+        organized_data[:, [21, 16]] = organized_data[:, [16, 21]]
+        organized_data[:, [22, 17]] = organized_data[:, [17, 22]]
+        organized_data[:, [23, 18]] = organized_data[:, [18, 23]]
+
+        # Front_angled_tube_rosette_stress
+        organized_data[:, [27, 19]] = organized_data[:, [19, 27]]
+        organized_data[:, [28, 20]] = organized_data[:, [20, 28]]
+        organized_data[:, [29, 21]] = organized_data[:, [21, 29]]
+
+        # Single strain gauges - stress
+        organized_data[:, [30, 22]] = organized_data[:, [22, 30]]
+        organized_data[:, [20, 23]] = organized_data[:, [23, 20]]
+        organized_data[:, [19, 24]] = organized_data[:, [24, 19]]
+
+        # Rosette_main_stresses
+        organized_data[:, [34, 25]] = organized_data[:, [25, 34]]
+        organized_data[:, [35, 26]] = organized_data[:, [26, 35]]
+        organized_data[:, [36, 27]] = organized_data[:, [27, 36]]
+
+        self.organized_stripped_data = organized_data[:, range(28)]
+
+    def calculations(self):
+        a_rear_tube = float(A_rear_tube)
+        a_chai_tube = float(A_chai_tube)
+        a_sitt_tube = float(A_sitt_tube)
+        a_horz_tube = float(A_horz_tube)
+        a_fron_tube = float(A_fron_tube)
+
+        self.internal_forces = np.array([]).reshape(0, 5)
+
+        for i in range(len(self.organized_stripped_data)):
+            self.internal_forces = np.append(self.internal_forces, [[float(self.organized_stripped_data[i, 24])
+                                                                     * a_rear_tube,
+                                                                     float(self.organized_stripped_data[i, 23])
+                                                                     * a_chai_tube,
+                                                                     float(self.organized_stripped_data[i, 26])
+                                                                     * a_sitt_tube,
+                                                                     float(self.organized_stripped_data[i, 14])
+                                                                     * a_horz_tube,
+                                                                     float(self.organized_stripped_data[i, 20])
+                                                                     * a_fron_tube]], axis=0)
+
+    def plotting(self):
+        horizontal_axis_internal_forces = self.organized_stripped_data[:, 0]
+        vertical_axis_internal_forces = [self.internal_forces[:, 0], self.internal_forces[:, 1],
+                                         self.internal_forces[:, 2], self.internal_forces[:, 3],
+                                         self.internal_forces[:, 4]]
+        plot_title = ['Internal Force Rear-most Tube, [N]', 'Internal Force Tube Parallel To Chain, [N]',
+                      'Internal Force Sitting Tube, [N]', 'Internal Force Horizontal Tube, [N]',
+                      'Internal Force Front Angled Tube, [N]']
+        plt.figure(1, figsize=(20, 13))
+        for i in range(5):
+            plt.subplot(5, 1, i + 1)
+            plt.scatter(horizontal_axis_internal_forces, vertical_axis_internal_forces[i], s=1, marker=',')
+            plt.grid(True)
+            plt.title(plot_title[i])
+            plt.xticks([])
+        plt.tight_layout()
+        plt.show()
+
+
+# Plot settings
+np.set_printoptions(linewidth=400, edgeitems=18, suppress=True)
+
+# Classes
+data = Data()
+
+# Functions
+data.organizing()
+data.calculations()
+data.plotting()
